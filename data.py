451de8d12ead7de412b1448f7ce21468ed624708
Yen Tran <yennatrann@gmail.com>
2024-01-30 09:04:36 -0500

    article-blog

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sem augue, lobortis a diam vitae, tincidunt tempus enim. In lacinia ipsum eros, quis porttitor nulla rutrum vel. Etiam eleifend leo non consectetur lobortis. Donec ultrices tortor sit amet efficitur pulvinar. Donec augue velit, egestas vel sagittis in, aliquet vitae ante. Mauris sit amet volutpat enim. Donec a ligula vitae sapien placerat pellentesque a vel est. Nam bibendum vel metus id venenatis.',
+            'author': 'Yenna Tran',
+            'create_date': '02-19-2022'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sem augue, lobortis a diam vitae, tincidunt tempus enim. In lacinia ipsum eros, quis porttitor nulla rutrum vel. Etiam eleifend leo non consectetur lobortis. Donec ultrices tortor sit amet efficitur pulvinar. Donec augue velit, egestas vel sagittis in, aliquet vitae ante. Mauris sit amet volutpat enim. Donec a ligula vitae sapien placerat pellentesque a vel est. Nam bibendum vel metus id venenatis.',
+            'author': 'John Doe',
+            'create_date': '02-19-2022'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sem augue, lobortis a diam vitae, tincidunt tempus enim. In lacinia ipsum eros, quis porttitor nulla rutrum vel. Etiam eleifend leo non consectetur lobortis. Donec ultrices tortor sit amet efficitur pulvinar. Donec augue velit, egestas vel sagittis in, aliquet vitae ante. Mauris sit amet volutpat enim. Donec a ligula vitae sapien placerat pellentesque a vel est. Nam bibendum vel metus id venenatis.',
+            'author': 'Yenna Tran',
+            'create_date': '02-19-2022'
+        }
+    ]
+
+    return articles
